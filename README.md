26e000309d8016c5c58f127b7b25b293f9fb47f4
Clive Young <yang.clive@gmail.com>
2015-02-04 16:27:40 +0800

    add ValidationTrait, no tests

---
+++ README.md
@@ -0,0 +1,134 @@
+Laravel Validation Trait
+================================================
+
+Provide a powerful trait for laravel 4 model
+
+### Use it in Eloquent
+
+create a model and use it
+
+```php
+
+use Yangmls\ValidationTrait;
+
+class User extends Eloquent
+{
+    use ValidationTrait;
+
+    public function rules()
+    {
+        return [
+            'email' => ['required', 'email'],
+        ];
+    }
+
+    public function rulesMessages()
+    {
+        return [
+            'email.required' => ':attribute is required',
+        ];
+    }
+
+    public function customAttributes()
+    {
+        return [
+            'email' => 'E-Mail',
+        ];
+    }
+}
+
+```
+
+create a new model and save
+
+```php
+
+$model = new User();
+$result = $model->saveFromRequest(Input::all()); // true or false
+return Response::json($model->getErrors());
+
+```
+
+create a new model without errors
+
+```php
+
+$model = User::createFromRequest(Input::all()); // User instance or null
+
+```
+
+save a existing model
+
+```php
+
+$model = User::find(1);
+$result = $model->saveFromRequest(Input::all()); // true or false
+return Response::json($model->getErrors());
+
+```
+
+### Use it in other models
+
+sometimes you may process a form without Eloquent, you can do like this
+
+```php
+
+use Yangmls\ValidationTrait;
+
+class Login
+{
+    use ValidationTrait;
+
+    public $attributes;
+
+    public function __construct($input = [])
+    {
+        $this->attributes = $input;
+    }
+    
+    public function validate($options = [])
+    {
+        return $this->validateRequest($this->attributes, $options);
+    }
+
+    public function rules()
+    {
+        return [
+            'email' => ['required', 'email'],
+        ];
+    }
+
+    public function rulesMessages()
+    {
+        return [
+            'email.required' => ':attribute is required',
+        ];
+    }
+
+    public function customAttributes()
+    {
+        return [
+            'email' => 'E-Mail',
+        ];
+    }
+}
+
+```
+
+then call it in controller
+
+```php
+
+$model = new Login(Input::all());
+$result = $model->validate(); // true or false
+return Response::json($model->getErrors());
+
+```
+
+### Built-in hooks
+
+below methods are called automatically when you do validating or saving
+
+`beforeSave`, `afterSave`, `beforeValidate`, `afterValidate`
+
+however you can use laravel built-in events to do this too
---
+++ src/Yangmls/ValidationTrait.php
@@ -0,0 +1,187 @@
+<?php
+namespace Yangmls;
+
+use Illuminate\Support\MessageBag,
+    Validator;
+
+/**
+ * Validation Trait that provides methods to create and update model easily
+ */
+trait ValidationTrait
+{
+    /**
+     * message bag
+     *
+     * @var \Illuminate\Support\MessageBag 
+     */
+    protected $errors;
+
+    /**
+     * create model by request data
+     * 
+     * @param array $input
+     * @param array $options
+     * @return null|static
+     */
+    public static function createFromRequest($input = [], $options = [])
+    {
+        $model = new static();
+        return $model->saveFromRequest($input, $options) ? $model : null;
+    }
+    
+    /**
+     * update model by request data
+     * 
+     * @param array $input
+     * @param array $options
+     * @return boolean
+     */
+    public function updateFromRequest($input = [], $options = [])
+    {
+        return $this->saveFromRequest($input, $options);
+    }
+    
+    /**
+     * save data by request data
+     * 
+     * @param array $input
+     * @param array $options
+     * @return boolean
+     */
+    public function saveFromRequest($input = [], $options = [])
+    {
+        if(!$this->validateRequest($input, $options)) {
+            return false;
+        }
+        foreach($input as $key=>$value) {
+            $this->setAttribute($key, $value);
+        }
+        
+        if(method_exists($this, 'beforeSave')) {
+            if(!$this->beforeSave($options)) {
+                return false;
+            }
+        }
+        $beforeExists = $this->exists;
+        $saved = $this->save();
+        $afterExists = $this->exists;
+        
+        if($saved && method_exists($this, 'afterSave')) {
+            $this->exists = $beforeExists;
+            $this->afterSave($options);
+            $this->exists = $afterExists;
+        }
+
+        return $saved;
+    }
+    
+    /**
+     * validate current model by request data
+     * 
+     * @param array $input
+     * @param array $options
+     * @return boolean
+     */
+    public function validateRequest(&$input, $options = [])
+    {
+        if(method_exists($this, 'beforeValidate')) {
+            if(!$this->beforeValidate($input, $options)) {
+                return false;
+            }
+        }
+        
+        $rules = method_exists($this, 'rules') ? $this->rules() : [];
+        $ruleMessages = method_exists($this, 'ruleMessages') ? $this->ruleMessages() : [];
+        $customAttributes = method_exists($this, 'customAttributes') ? $this->customAttributes() : [];
+        
+        $keys = [];
+        
+        //defined in rules
+        if(!empty($rules)) {
+            $validation = Validator::make($input, $rules, $ruleMessages, $customAttributes);
+            $this->getErrors(true)->merge($validation);
+            foreach(array_keys($keys) as $key) {
+                if(!$validation->errors()->has($key)) {
+                    $keys[] = $key;
+                }
+            }
+        }
+        
+        //inline validators
+        foreach($input as $key=>$value) {
+            $validator = 'validator' . ucfirst($key);
+            if(!method_exists($this, $validator)) {
+                continue;
+            }
+            if($this->$validator($value, $input, $options)) {
+                $keys[] = $key;
+            }
+        }
+        $errors = $this->getErrors(true);
+        //you can pass ignore as option, so you will get input with passed keys
+        if(!$errors->isEmpty() && !array_get($options, 'ignore')) {
+            return false;
+        }
+        $fillable = property_exists($this, 'fillable') ? $this->fillable : [];
+        $fillable = array_merge($fillable, $keys);
+        $input = array_only($input, $fillable);
+        
+        if(method_exists($this, 'afterValidate')) {
+            $this->afterValidate($input, $options);
+        }
+        
+        return true;
+    }
+    
+    /**
+     * add error message to message bag
+     * 
+     * @param string $key
+     * @param string $message
+     */
+    public function addError($key, $message)
+    {
+        $this->getErrors(true)->add($key, $message);
+    }
+    
+    /**
+     * get all messages from message bag
+     * 
+     * @param boolean $raw
+     * @return mixed
+     */
+    public function getErrors($raw = false)
+    {
+        if($this->errors === null) {
+            $this->errors = new MessageBag();
+        }
+        
+        if($raw) {
+            return $this->errors;
+        }
+        return $this->errors->getMessages();
+    }
+    
+    /**
+     * get single error message from message bag
+     * 
+     * @param string $key
+     * @return string
+     */
+    public function getError($key = null)
+    {
+        $errors = $this->getErrors(true);
+        return $errors->first($key);
+    }
+    
+    /**
+     * if message bag has errors
+     * 
+     * @return boolean
+     */
+    public function hasErrors()
+    {
+        $errors = $this->getErrors(true);
+        return !$errors->isEmpty();
+    }
+}
